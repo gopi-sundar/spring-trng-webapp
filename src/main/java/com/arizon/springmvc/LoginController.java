f1877e1fd0fe35af94ecb323970e39c457e06956
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-18 10:52:08 -0600

    b8-view-resolver

--- src/main/java/com/arizon/springmvc/LoginController.java
+++ src/main/java/com/arizon/springmvc/LoginController.java
@@ -2,15 +2,13 @@ package com.arizon.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login")
-	@ResponseBody
-	public String sayHello() {
-		return "Hello World !!!";
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage() {
+		return "login";
 	}
-
 }
