7b6afe51bc725f026fc2f91050865ad3bea6aae7
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-18 14:43:08 -0600

    b12-login-service-di

--- src/main/java/com/arizon/login/LoginController.java
+++ src/main/java/com/arizon/login/LoginController.java
@@ -1,18 +1,18 @@
 package com.arizon.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.arizon.login.LoginService;
-
 
 @Controller
 public class LoginController {
 
-	private LoginService loginService = new LoginService();
+	@Autowired
+	private LoginService loginService;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
--- src/main/java/com/arizon/login/LoginService.java
+++ src/main/java/com/arizon/login/LoginService.java
@@ -1,5 +1,8 @@
 package com.arizon.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 	public boolean validateUser(String user, String password) {
 		return user.equalsIgnoreCase("arizon") && password.equals("test");
