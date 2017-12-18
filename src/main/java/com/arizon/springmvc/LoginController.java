db2580ad5141c8d6f737b0a1c88ed4abf2aef784
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-12-18 12:18:01 -0600

    b9-log4j

--- src/main/java/com/arizon/springmvc/LoginController.java
+++ src/main/java/com/arizon/springmvc/LoginController.java
@@ -1,5 +1,6 @@
 package com.arizon.springmvc;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
+	private static Logger logger = Logger.getLogger(LoginController.class);
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
+
+		logger.trace("Inside showLoginPage ");
+		logger.debug("Inside showLoginPage ");
+		logger.info("Inside showLoginPage ");
+		logger.warn("Inside showLoginPage ");
+		logger.error("Inside showLoginPage ");
 		return "login";
 	}
 }
